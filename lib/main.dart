3f910e990cd9416422635f88d6b9dfbfb4ef2643
elselawi <ali.a.saleem@outlook.com>
2025-01-25 10:02:49 +0300

    turn on sentry only in release mode

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:apexo/app/app.dart';
 import 'package:apexo/sentry_dsn.dart';
 import 'package:apexo/utils/init_stores.dart';
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:logging/logging.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
@@ -19,12 +20,16 @@ void main() async {
     systemNavigationBarIconBrightness: Brightness.dark,
   ));
 
-  await SentryFlutter.init(
-    (options) {
-      options.dsn = sentryDSN;
-      // options.tracesSampleRate = 1.0;
-      // options.profilesSampleRate = 1.0;
-    },
-    appRunner: () => runApp(const ApexoApp()),
-  );
+  if (kDebugMode) {
+    runApp(const ApexoApp());
+  } else {
+    await SentryFlutter.init(
+      (options) {
+        options.dsn = sentryDSN;
+        // options.tracesSampleRate = 1.0;
+        // options.profilesSampleRate = 1.0;
+      },
+      appRunner: () => runApp(const ApexoApp()),
+    );
+  }
 }
