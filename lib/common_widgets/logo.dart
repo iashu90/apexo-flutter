afd666867204e07bd5f9ad976faca5b0efd7e93d
Ashraf Ismail <iashu90@gmail.com>
2025-07-10 11:04:41 -0700

    fix: Update displayed date in AppLogo and handle corrupt data in Store model serialization

--- lib/common_widgets/logo.dart
+++ lib/common_widgets/logo.dart
@@ -53,7 +53,7 @@ class _AppLogoState extends State<AppLogo> {
                 Text(version, style: textStyle),
                 const SizedBox(height: 2),
                 Text(
-                  "09 July 2025",
+                  "11 July 2025",
                   style: textStyle,
                 ),
               ],
