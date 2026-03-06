95de50e7e14befa713cb93127db3e3f5089d3b88
Ashraf Ismail <iashu90@gmail.com>
2026-03-05 23:54:21 -0800

    fix: Update displayed date in AppLogo to reflect the correct release date

--- lib/common_widgets/logo.dart
+++ lib/common_widgets/logo.dart
@@ -53,7 +53,7 @@ class _AppLogoState extends State<AppLogo> {
                 Text(version, style: textStyle),
                 const SizedBox(height: 2),
                 Text(
-                  "9 August 2025",
+                  "6 March 2026",
                   style: textStyle,
                 ),
               ],
